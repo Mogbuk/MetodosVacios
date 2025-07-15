cc07ce152f0f522aacc295f75f9d308c173fa63a
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:40:24 -0500

    Ejercicio #4

---
+++ Cumpleaños.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Cumpleaños
+    {
+        public static void FelizCumpleaños(){
+            Console.Write("\nFELIZ CUMPLEAÑOS, ESPERO QUE LA ESTES PASANDO BIEN Y QUE CUMPLAS MUCHOS MAS!\n");
+        }
+    }
+}
