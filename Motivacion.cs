66ad317a4a5c168a0a3431cb4f9bfc6de65bee10
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:58:12 -0500

    Ejercicio #7

---
+++ Motivacion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Motivacion
+    {
+        public static void MensajeMotivacional(){
+            Console.Write("\n¡TU PUEDES LOGRAR TODO LO QUE TE PROPONGAS!\n");
+        }
+    }
+}
