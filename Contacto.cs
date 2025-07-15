213b1b7dba0d0f52ded6b13113c875f6e502ae33
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:52:51 -0500

    Ejercicio #6

---
+++ Contacto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Contacto
+    {
+        public static void InfoContacto(){
+            Console.Write("\nEste es tu información de contacto: \n" +
+                "\nNombre: Alexander Volkanovski" +
+                "\nEmail: AlexanderVolkanovski@gmail.com" +
+                "\nTeléfono: 123456789\n");
+        }
+    }
+}
