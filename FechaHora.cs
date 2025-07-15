cad8c1ff83cc9d3959334a93f09448fa6a63128f
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:32:30 -0500

    Ejercicio #3

---
+++ FechaHora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class FechaHora
+    {
+        public static void FechaYHora(){
+            Console.Write("\nLa fecha y la hora exacta es: {0}\n", DateTime.Now);
+        }
+    }
+}
