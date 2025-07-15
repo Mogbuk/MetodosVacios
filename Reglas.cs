f55c0fb1c559b654561bce9640c3c03a3e211e03
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 16:05:45 -0500

    Ejercicio #8

---
+++ Reglas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Reglas
+    {
+        public static void ReglasUso(){
+            Console.Write("\nEstas son las siguientes reglas de uso: \n" +
+                "\n1. Vamos a ingresar al sistema" +
+                "\n2. Vamos a registrarnos" +
+                "\n3. Ingresar con el usuario creado\n");
+        }
+    }
+}
