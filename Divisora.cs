ee3f56e8ff3b96d3371251f3f73f2a2068154734
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:48:59 -0500

    Ejercicio #5

---
+++ Divisora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Divisora
+    {
+        public static void LineaDivisora(){
+            Console.Write("\n----------------------------------\n");
+        }
+    }
+}
