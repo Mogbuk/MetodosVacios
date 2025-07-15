b00725fc763b864b2894b015b7a9ebb95e0aa626
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 16:13:39 -0500

    Ejercicio #10

---
+++ Creditos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Creditos
+    {
+        public static void CreditosPrograma(){
+            Console.Write("\nAlexander Volkanovski | 2025\n");
+        }
+    }
+}
