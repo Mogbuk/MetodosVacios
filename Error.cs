e4b8cbdecde406ade1ff013a3faf4fc5e9d1cd29
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 16:08:08 -0500

    Ejercicio #9

---
+++ Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Error
+    {
+        public static void MensajeError(){
+            Console.Write("\nHa ocurrido un error inesperado. Inténtalo de nuevo\n");
+        }
+    }
+}
