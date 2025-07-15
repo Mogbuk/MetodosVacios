a170d925b767bbb56600266912bf5ae23151a936
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:36:04 -0500

    Ejercicio #3

---
+++ Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class Menu
+    {
+        public static void MenuColores(){
+            Console.Write("\nEste es el menú de colores: \n" +
+                "\n1. Rojo" +
+                "\n2. Azul" +
+                "\n3. Verde\n");
+        }
+    }
+}
