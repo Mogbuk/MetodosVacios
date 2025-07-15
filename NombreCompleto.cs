f9d40ef1dc3668d1d7064f81857852b076d9e012
Mogbuk <xsebastianrojasx@gmail.com>
2025-07-15 15:28:25 -0500

    Ejercicio #1

---
+++ NombreCompleto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosVacios1
+{
+    internal class NombreCompleto
+    {
+        public static void NombreApellido(){
+            Console.Write("\nMi nombre completo es: Alexander Volkanovski\n");
+        }
+    }
+}
